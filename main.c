2a433ff5ce1f71896b17fea50d058223466ac939
k16858 <ktohda0127@gmail.com>
2026-02-28 00:02:53 +0900

    実行ループを追加

--- main.c
+++ main.c
@@ -197,12 +197,23 @@ int step(struct state *state) {
   return 0;
 }
 
+void run(struct state *state) {
+  while (state->running) {
+    if (step(state) != 0) {
+      printf("CPU Panic: Exception occurred at PC=0x%04X\n", state->pc);
+      break;
+    }
+  }
+}
+
 int main(void) {
   static struct state state;
   init_state(&state);
   state.mem[0] = 0b0000000000000111;
   state.mem[1] = 0b0100000000000000;
+  state.mem[1] = 0b1111100000000000;
 
-  step(&state);
-  step(&state);
+  run(&state);
+
+  return 0;
 }
