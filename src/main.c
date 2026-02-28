b9c93d303792ea6db11d695d596f3ce77fd01667
k16858 <ktohda0127@gmail.com>
2026-02-28 22:10:52 +0900

    メモリファイル読み込み関数を追加

--- src/main.c
+++ src/main.c
@@ -1,10 +1,36 @@
 #include "cpu.h"
 #include "emulator.h"
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+
+#define INST_LEN 6
+
+int load_machinelang(char *file, uint16_t *mem[]) {
+  FILE *fp = fopen(file, "r");
+
+  if (fp == NULL) {
+    printf("Error: Cannot open file: %s\n", file);
+    return 1;
+  }
+
+  char buf[INST_LEN + 1];
+  char *endp;
+  int i = 0;
+
+  while (fgets(buf, INST_LEN, fp)) {
+    *mem[i] = (uint16_t)strtol(buf, &endp, 0);
+    i++;
+  }
+
+  return 0;
+}
 
 void run(struct state *state) {
   while (state->running) {
