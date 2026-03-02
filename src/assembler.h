047995543aecb35539685e356af71f339b046209
k16858 <ktohda0127@gmail.com>
2026-03-02 22:45:01 +0900

    パース結果構造体とパース関数を宣言

--- src/assembler.h
+++ src/assembler.h
@@ -2,12 +2,23 @@
 #define CPU_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 struct table {
     char *name;
     uint16_t address;
 };
 
+struct parsed_line {
+    char *operation;
+    int regs[3];
+    int imm;
+    char *target_label;
+    char *label_name;
+    bool is_label;
+};
+
 int tokenize_line(char *line, char *ret[]);
+int parse_line(char *line, struct parsed_line parse_result);
 
 #endif
